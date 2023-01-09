f1d0a76787bdae35ba2aca93f11f9647b7356a94
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2023-01-09 17:23:05 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,15 @@ public class Main
     // Variable
     private static Scanner              sc;
     private static int                  nbrEtudiantGp;
-    private static String               tempsExam;
+    private static String[]             tempsExam;
 
     private static ArrayList<Etudiant>  listEtudiant;
     private static ArrayList<Salle>     listSalle;
     private static ArrayList<Jury>      listJury;
 
     private static String[][] equipeEtudiant;
+
+    private static Outils outils = new Outils();
 
     public static void main(String[] args) throws IOException, Exception
     {
@@ -37,12 +39,12 @@ public class Main
 
         //Lecture du fichier jury.data
         sc = new Scanner(new FileInputStream("../lib/jury.data"));
-        tempsExam = sc.nextLine();
+        tempsExam = outils.decomposeur(sc.nextLine(), 9, ":");
 
         while(sc.hasNextLine())
             listJury.add(new Jury(sc.nextLine()));
 
-        affichageConsole1();
+        affichageConsole2();
     }
 
     //Récupération de l'ensemble des catégories utiliser
@@ -148,22 +150,42 @@ public class Main
     //Affiche les jurys et les équipes attribuer avec leurs horaires
     public static void affichageConsole2() throws Exception
     {
-        Date date;
         SimpleDateFormat format;
-        int cptEquipe = 1;
-        //String[][] horaires;
+        int cptEquipe = 0;
+        Date heureActuelle, heureFin;
+        boolean bcl;
+        Date dateActuelle = new SimpleDateFormat("dd/MM/yyyy").parse(listJury.get(0).getDate());
+        Date datePrecedente = dateActuelle;
+        Date tmp;
+        Calendar cal;
+
+        format = new SimpleDateFormat("EEEE d MMMMMMMMMM yyyy");
+        for(int j =0; j < format.format(dateActuelle).length(); j++)
+            System.out.print("-");
+        System.out.println("");
+        System.out.println(format.format(dateActuelle));
+        for(int j =0; j < format.format(dateActuelle).length(); j++)
+            System.out.print("-");
 
         for(int i = 0; i < listJury.size(); i++)
         {
-            format = new SimpleDateFormat("EEEE d MMMMMMMMMM yyyy");
-            date = new SimpleDateFormat("dd/MM/yyyy").parse(listJury.get(i).getDate());
+            dateActuelle = new SimpleDateFormat("dd/MM/yyyy").parse(listJury.get(i).getDate());
 
-            for(int j =0; j < format.format(date).length(); j++)
-                System.out.print("-");
-            System.out.println("");
-            System.out.println(format.format(date));
-            for(int j =0; j < format.format(date).length(); j++)
-                System.out.print("-");
+            if(dateActuelle.compareTo(datePrecedente) != 0)
+            {
+                format = new SimpleDateFormat("EEEE d MMMMMMMMMM yyyy");
+                dateActuelle = new SimpleDateFormat("dd/MM/yyyy").parse(listJury.get(i).getDate());
+
+                System.out.println("\n");
+                for(int j =0; j < format.format(dateActuelle).length(); j++)
+                    System.out.print("-");
+                System.out.println("");
+                System.out.println(format.format(dateActuelle));
+                for(int j =0; j < format.format(dateActuelle).length(); j++)
+                    System.out.print("-");
+            }
+
+            datePrecedente = dateActuelle;
 
             System.out.println("");
             System.out.println("");
@@ -171,12 +193,49 @@ public class Main
             System.out.println(listJury.get(i).getJuryId());
             for(int m = 0; m < listJury.get(i).getProf().size(); m++)
                 System.out.println(listJury.get(i).getProf().get(m));
-            System.out.println(tempsExam);
 
-            //Séparer heure et minute dans un tableau deux dimension
-            /*horaires = new String[listJury.get(i).getHoraires().size()][2];
-              for(int l = 0; l < listJury.get(i).getHoraires().size(); l++)
-                horaires[l] = listJury.get(i).getHoraires().get(l).split(":");*/
+            format = new SimpleDateFormat("hh:mm");
+
+            heureActuelle = format.parse(listJury.get(i).getHoraires().get(0));
+            heureFin = format.parse(listJury.get(i).getHoraires().get(1));
+
+            bcl = true;
+            
+            while(bcl)
+            {
+                cptEquipe++;
+                try
+                {
+                    System.out.println("");
+                    System.out.print(String.format("%5s", outils.decomposeur(format.format(heureActuelle), (int)((char)':'), ":")[0] + "h"
+                                     + outils.decomposeur(format.format(heureActuelle), (int)((char)':'), ":")[1] ));
+
+                    //Calcule de l'intervale de passage
+                    cal = Calendar.getInstance();
+                    cal.setTime(heureActuelle);
+                    cal.add(Calendar.MINUTE, 25);
+                    heureActuelle = cal.getTime();
+
+                    System.out.print(" à " +
+                                     String.format("%1s", outils.decomposeur(format.format(cal.getTime()), (int)((char)':'), ":")[0] + "h"
+                                     + outils.decomposeur(format.format(cal.getTime()), (int)((char)':'), ":")[1] ));
+                    System.out.print(String.format("%9s", " équipe " + cptEquipe));
+                    System.out.print(String.format("%9s", " salle " + listJury.get(i).getSalle()));
+
+                    cal = Calendar.getInstance();
+                    cal.setTime(heureActuelle);
+                    cal.add(Calendar.MINUTE, 25);
+                    tmp = cal.getTime(); 
+
+                    if(tmp.compareTo(heureFin) > 0)
+                        bcl = false;
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("\n");
         }
     }
 }
